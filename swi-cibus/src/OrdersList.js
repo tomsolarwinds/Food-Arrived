c0a6fde951f4471903c75039f2665252b9c73f1b
Ran Endelman <ran.endelman@samanage.com>
2022-05-24 13:33:12 +0300

    init frontend

---
+++ swi-cibus/src/OrdersList.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import List from '@mui/material/List';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import OrderItem from './OrderItem'
+import useApi from './hooks/useApi';
+
+const style = {
+  position: 'relative',
+  marginTop: '60px',
+  bgcolor: 'background.paper',
+};
+
+const OrdersList = () =>  {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+  const api = useApi();
+
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true);
+      const response = await api.get('https://randomuser.me/api/?results=10.', {
+        params: {
+        },
+      });
+      setData(response?.data?.results);
+      setLoading(false);
+    };
+    fetch();
+  }, [api]);
+
+  if (loading) return (
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '100px' }}>
+      <CircularProgress />
+    </Box>
+  )
+
+  return (
+    <List style={style}>
+      {data?.map((item) => <OrderItem key={item?.email} item={item} />)}
+    </List>
+  );
+}
+
+export default OrdersList;
